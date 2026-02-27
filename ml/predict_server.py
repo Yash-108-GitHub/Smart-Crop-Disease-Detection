59b8a656e282b701699caa074187de4fb2309a53
Yash Barahate <yashmeena00108@gmial.com>
2026-02-27 17:01:44 +0530

    hi

--- ml/predict_server.py
+++ ml/predict_server.py
@@ -5,6 +5,11 @@ import numpy as np
 from io import BytesIO
 import os
 
+import json
+with open("labels.json", "r") as f:
+    class_labels = json.load(f)
+
+    
 app = Flask(__name__)
 
 # Load trained model
